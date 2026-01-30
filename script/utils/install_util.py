1c8afc8caa46893e90f14d81c783779e7183d964
Crystal0404 <120556221+Crystal0404@users.noreply.github.com>
2026-01-30 22:44:09 +0800

    Fix If projects are not found, the installation will not be considered a failure

--- script/utils/install_util.py
+++ script/utils/install_util.py
@@ -109,7 +109,7 @@ class Install:
                 process.stdin.write("n\n")
                 process.stdin.flush()
             logger.info(text)
-            if re.match("Failed to add project:.*", text):
+            if re.match("Failed to add project:.*", text) or text == "No projects found!":
                 is_successful = False
         process.wait()
         return is_successful
